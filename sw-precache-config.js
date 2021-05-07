c58d98649f85cc27a38d9f6487249b8659289764
Sherry <sherryyue.c@protonmail.com>
2021-05-07 13:36:58 +0800

    create CouponShare

---
+++ sw-precache-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    staticFileGlobs: [
+        './',
+        './img/**.*',
+        './js/**.*',
+        './app.js',
+        './style.css',
+        './index.html'
+    ],
+    runtimeCaching: [{
+        urlPattern: /.*/,
+        handler: 'networkFirst'
+    }],
+    swFile: 'service-worker.js'
+};
